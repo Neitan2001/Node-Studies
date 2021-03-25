c56bf73385ab82c57c2f66443fb13d541300c767
Neitan2001 <natanestudante2001@hotmail.com>
2021-03-25 18:18:37 -0300

    Lesson 9

--- First-App/app.js
+++ First-App/app.js
@@ -1,33 +1,46 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
 
 //express app
 const app = express();
+
+//Connect to mongoDB
+const dbURI = 'mongodb+srv://neitan2001:lilicabonita123@nodestudies.qmt6r.mongodb.net/nodeStudies?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
 
 // register view engine
 
 app.set('view engine', 'ejs');
 
-//listen for requests
-app.listen(3000);
 
 //middleware & static files
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+
+//routes
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Banana', snippet: 'How to eat banana' },
-        { title: 'Apple', snippet: 'How to eat Apple' },
-        { title: 'Grape', snippet: 'How to eat Grape' },
-    ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
 
     res.render('about', { title: 'About' });
+});
+
+//blog create
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 }).then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result })
+    }).catch((err) => {
+        console.log(err);
+    })
 });
 
 app.get('/blogs/create', (req, res) => {
